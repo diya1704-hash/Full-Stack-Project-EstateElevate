c06c851d686a3e7901a38dc76ee097429a75cd09
diya1704-hash <diyaramawat17@gmail.com>
2025-12-19 21:12:24 +0530

    Final Fix

--- app.py
+++ app.py
@@ -1,54 +1,51 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
+import pymysql # Changed: Using pymysql for Vercel
 from werkzeug.utils import secure_filename
 from PIL import Image
 
 app = Flask(__name__)
 app.secret_key = 'flipr_ultra_secret'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-# Database Config (Update with your credentials)
-# Use environment variables for production
-# Updated Database Config in app.py
-# Database Configuration for Aiven
-app.config['MYSQL_HOST'] = 'mysql-608471f-diyaramawat17-b50b.k.aivencloud.com'
-app.config['MYSQL_USER'] = 'avnadmin'
-import os
-# Use 'AVNS_...' as a backup for local testing, but look for the environment variable first
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'AVNS_T820t72lxjaujRHWlrc')
-app.config['MYSQL_DB'] = 'defaultdb'
-app.config['MYSQL_PORT'] = 16633
+# Database Configuration
+MYSQL_HOST = 'mysql-608471f-diyaramawat17-b50b.k.aivencloud.com'
+MYSQL_USER = 'avnadmin'
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'AVNS_T820t72lxjaujRHWlrc')
+MYSQL_DB = 'defaultdb'
+MYSQL_PORT = 16633
 
-# This is the "Secret Sauce" for Aiven - SSL is REQUIRED
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['MYSQL_CUSTOM_OPTIONS'] = {
-    "ssl": {
-        "ca": os.path.join(basedir, "ca.pem")
-    }
-}
+# Minimal change: This creates a 'mysql' object that behaves like your old one
+class MySQLWrapper:
+    @property
+    def connection(self):
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        conn = pymysql.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DB,
+            port=MYSQL_PORT,
+            ssl={'ca': os.path.join(basedir, "ca.pem")},
+            autocommit=True # Ensures data is saved immediately
+        )
+        return conn
 
-mysql = MySQL(app)
+mysql = MySQLWrapper()
 
 def save_and_crop(file, subfolder):
-    """Crops image to 450x350 ratio and saves it"""
     target_path = os.path.join(app.config['UPLOAD_FOLDER'], subfolder)
     if not os.path.exists(target_path):
         os.makedirs(target_path)
-        
     filename = secure_filename(file.filename)
     full_path = os.path.join(target_path, filename)
     file.save(full_path)
-    
     with Image.open(full_path) as img:
         img = img.convert('RGB')
         target_w, target_h = 450, 350
         width, height = img.size
-        
-        # Calculate cropping
         target_ratio = target_w / target_h
         current_ratio = width / height
-
         if current_ratio > target_ratio:
             new_width = int(target_ratio * height)
             offset = (width - new_width) / 2
@@ -57,22 +54,18 @@ def save_and_crop(file, subfolder):
             new_height = int(width / target_ratio)
             offset = (height - new_height) / 2
             img = img.crop((0, offset, width, height - offset))
-
         img = img.resize((target_w, target_h), Image.Resampling.LANCZOS)
         img.save(full_path)
-    
     return f'uploads/{subfolder}/{filename}'
 
 @app.route('/admin')
 def admin():
-    cur = mysql.connection.cursor()
-    # Fetch Counts for Dashboard Cards
+    conn = mysql.connection
+    cur = conn.cursor()
     cur.execute("SELECT COUNT(*) FROM contact_submissions")
     lead_count = cur.fetchone()[0]
     cur.execute("SELECT COUNT(*) FROM subscribers")
     sub_count = cur.fetchone()[0]
-    
-    # Fetch Table Data
     cur.execute("SELECT * FROM contact_submissions ORDER BY submitted_at DESC")
     leads = cur.fetchall()
     cur.execute("SELECT * FROM subscribers")
@@ -82,7 +75,7 @@ def admin():
     cur.execute("SELECT * FROM clients")
     clients = cur.fetchall()
     cur.close()
-    
+    conn.close()
     return render_template('admin.html', leads=leads, subs=subs, 
                            projects=projects, clients=clients,
                            lead_count=lead_count, sub_count=sub_count)
@@ -90,30 +83,33 @@ def admin():
 @app.route('/admin/add_project', methods=['POST'])
 def add_project():
     img_path = save_and_crop(request.files['image'], 'projects')
-    cur = mysql.connection.cursor()
+    conn = mysql.connection
+    cur = conn.cursor()
     cur.execute("INSERT INTO projects (name, description, image_path) VALUES (%s, %s, %s)", 
                 (request.form['name'], request.form['desc'], img_path))
-    mysql.connection.commit()
+    conn.close()
     return redirect(url_for('admin'))
 
 @app.route('/admin/add_client', methods=['POST'])
 def add_client():
     img_path = save_and_crop(request.files['image'], 'clients')
-    cur = mysql.connection.cursor()
+    conn = mysql.connection
+    cur = conn.cursor()
     cur.execute("INSERT INTO clients (name, description, designation, image_path) VALUES (%s, %s, %s, %s)", 
                 (request.form['name'], request.form['desc'], request.form['designation'], img_path))
-    mysql.connection.commit()
+    conn.close()
     return redirect(url_for('admin'))
 
-# Main Landing Page Route
 @app.route('/')
 def index():
-    cur = mysql.connection.cursor()
+    conn = mysql.connection
+    cur = conn.cursor()
     cur.execute("SELECT * FROM projects")
     projects = cur.fetchall()
     cur.execute("SELECT * FROM clients")
     clients = cur.fetchall()
     cur.close()
+    conn.close()
     return render_template('index.html', projects=projects, clients=clients)
 
 if __name__ == '__main__':
